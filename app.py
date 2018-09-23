4562f122032b4628c9137a31222ae5df052ee40f
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-09-23 22:03:14 +0100

    update

--- app.py
+++ app.py
@@ -2,15 +2,25 @@ from flask import Flask, g, jsonify, render_template
 from flask_limiter import Limiter
 from flask_limiter.util import get_ipaddr
 
+from auth import auth
 from config import DEBUG, DEFAULT_RATE, HOST, PORT
 from models import initialize
+from resources.todos import todo_api
 
 app = Flask(__name__)
+app.register_blueprint(todo_api, url_prefix='/api/v1/')
 
 
 @app.route('/')
 def my_todos():
     return render_template('index.html')
+
+
+@app.route('/api/v1/users/token', methods=['GET'])
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
 
 
 if __name__ == '__main__':
---
+++ resources/todos.py
@@ -0,0 +1,129 @@
+#: ATTENTION!! < # noinspection > prefixed comments are only
+#: for pycharm to ignore PEP 8 style highlights
+
+import json
+
+from flask import abort, Blueprint, g, make_response, url_for
+
+from flask_restful import (Api, fields, Resource, marshal,
+                           marshal_with, reqparse)
+
+from auth import auth
+from models import Todo
+
+
+todo_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'description': fields.String,
+    'created_at': fields.DateTime
+}
+
+
+def object_or_404(id):
+    try:
+        # noinspection PyUnresolvedReferences
+        # --> .id
+        todo = Todo.get(Todo.id == id)
+    except Todo.DoesNotExist:
+        abort(404)
+    else:
+        return todo
+
+
+class TodoList(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            required=True,
+            help='No todo title was provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'description',
+            required=True,
+            help='No description was provided',
+            location=['form', 'json'],
+        )
+        super().__init__()
+
+    @staticmethod
+    def get():
+        return [marshal(todo, todo_fields) for todo in Todo.select()]
+
+    @marshal_with(todo_fields)
+    @auth.login_required
+    def post(self):
+        args = self.reqparse.parse_args()
+        todo = Todo.create(created_by=g.user, **args)
+        return todo, 201, {'Location': url_for('resources.todos.todo', id=todo.id)}
+
+
+class Todos(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'name',
+            required=True,
+            help='No todo title was provided',
+            location=['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'description',
+            required=True,
+            help='No description was provided',
+            location=['form', 'json'],
+        )
+        super().__init__()
+
+    @marshal_with(todo_fields)
+    def get(self, todo_id):
+        return object_or_404(todo_id)
+
+    @marshal_with(todo_fields)
+    @auth.login_required
+    def put(self, todo_id):
+        args = self.reqparse.parse_args()
+        try:
+            # noinspection PyUnresolvedReferences
+            # --> .id
+            todo = Todo.select().where(
+                Todo.created_by == g.user,
+                Todo.id == todo_id
+            ).get()
+        except Todo.DoesNotExist:
+            return make_response(json.dumps(
+                    {'error': 'That todo does not exist or is not editable'}
+                ), 403)
+        query = todo.update(**args)
+        query.execute()
+        todo = object_or_404(todo_id)
+        return (todo, 200, {
+                'Location': url_for('resources.todos.todo', id=todo_id)})
+
+    @auth.login_required
+    def delete(self, todo_id):
+        try:
+            # noinspection PyUnresolvedReferences
+            # --> .id
+            todo = Todo.select().where(
+                Todo.created_by == g.user,
+                Todo.id == todo_id
+            ).get()
+        except Todo.DoesNotExist:
+            return make_response(json.dumps(
+                    {'error': 'That todo does not exist or is not editable'}
+                ), 403)
+        query = todo.delete()
+        query.execute()
+        return '', 204, {'Location': url_for('resources.todos.todo')}
+
+
+todo_api = Blueprint('resources.reviews', __name__)
+# noinspection PyTypeChecker
+api = Api(todo_api)
+# noinspection PyTypeChecker
+api.add_resource(TodoList, '/todos', endpoint='todos')
+# noinspection PyTypeChecker
+api.add_resource(Todos, '/todos/<int:id>', endpoint='todo')
