ba8a2e62e75d112073bba82186498d1d9ad107c9
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-09-25 20:08:50 +0100

    update

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import string
 from playhouse.test_utils import test_database
 
 from peewee import *
@@ -8,46 +9,35 @@ from models import Todo, User
 
 
 DB = SqliteDatabase(':memory:')
+DB.connect()
+DB.create_tables([User, Todo], safe=True)
 
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        DB.connect()
-        DB.create_tables([Todo, User], safe=True)
-
-    def tearDown(self):
-        DB.drop_tables(Todo, User)
-        DB.close()
-
-class UserTestCase(BaseTest):
-    ''' test cases for the user model '''
-    @staticmethod # static method as it does not access anything in the class
-    def create(count=2):
-        ''' this test creates 2 users in the database via a function called
-            create_users
-        '''
-        for i in range(count):
-            User.create_user(
-                username='user_{}'.format(i),
-                email='test_{}@example.com'.format(i),
-                password='password'
-            )
+class UserTestCase(unittest.TestCase):
+    @staticmethod
+    def create(prefix=None):
+        User.create_user(
+            username=f'{prefix}_user',
+            email=f'example_{prefix}@mail.com',
+            password='password'
+        )
 
 
-class TodoTestCase(BaseTest, UserTestCase):
-    # @staticmethod
-    def create(self):
-        # UserModelTestCase.create()
-        # user = User.select().get()
-        Todo.create(name='Walk Tomika', created_by=self.create())
-        # Todo.create(name='Clean Car', created_by=user.id)
+class TodoTestCase(unittest.TestCase):
+    @staticmethod
+    def create():
+        UserTestCase.create('test_2')
+        user = User.select().get()
+        Todo.create(name='Walk Tomika', created_by=user.id)
 
     def test_todo_create(self):
         with test_database(DB, (Todo,)):
-            Todo.create(
-                name='Walk Tomika', created_by=1
-            )
+            self.create()
             self.assertEqual(Todo.select().count(), 1)
+
+    def tearDown(self):
+        DB.drop_tables(User, Todo)
+        DB.close()
 
 
 if __name__ == '__main__':
