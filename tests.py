c194d726a5c4076c144cdc7966dcc7940430e6d9
Feher Oszkar <feheroszkar@yahoo.com>
2018-09-26 05:14:48 +0100

    update, tests

--- tests.py
+++ tests.py
@@ -9,35 +9,64 @@ from models import Todo, User
 
 
 DB = SqliteDatabase(':memory:')
-DB.connect()
-DB.create_tables([User, Todo], safe=True)
 
 
-class UserTestCase(unittest.TestCase):
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+        DB.connect()
+        DB.create_tables([User, Todo], safe=True)
+
+    def tearDown(self):
+        DB.drop_tables(User, Todo)
+        DB.close()
+
+
+class ModelResourcesTest(BaseTest):
     @staticmethod
-    def create(prefix=None):
+    def create_user(prefix=None):
         User.create_user(
             username=f'{prefix}_user',
             email=f'example_{prefix}@mail.com',
             password='password'
         )
 
+    def test_create_user(self):
+        with test_database(DB, (User, )):
+            self.create_user('test_1')
+            self.assertEqual(len(User.select()), 1)
+            response = self.app.get('/api/v1/users')
+            self.assertEqual(User.select().get().username, 'test_1_user')
+            self.assertEqual(response.status_code, 404)
 
-class TodoTestCase(unittest.TestCase):
-    @staticmethod
-    def create():
-        UserTestCase.create('test_2')
-        user = User.select().get()
-        Todo.create(name='Walk Tomika', created_by=user.id)
+    def test_create_new_user(self):
+        data = {
+            'username': 'rayan',
+            'email': 'example@mail.com',
+            'password': 'password',
+            'verify_password': 'password'
+        }
 
-    def test_todo_create(self):
+        with test_database(DB, (User,)):
+            response = self.app.post('/api/v1/users', data=data)
+            self.assertEqual(response.status_code, 404)
+
+    def test_create_todo(self):
         with test_database(DB, (Todo,)):
-            self.create()
-            self.assertEqual(Todo.select().count(), 1)
+            self.create_user('test_2')
+            user = User.select().get()
+            Todo.create(name='Walk Tomika', created_by=user.id)
+            self.assertEqual(len(Todo.select()), 1)
+            response = self.app.post('/api/v1/todos')
+            self.assertEqual(response.status_code, 404)
 
-    def tearDown(self):
-        DB.drop_tables(User, Todo)
-        DB.close()
+
+class ViewTest(BaseTest):
+    def test_my_todos(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
